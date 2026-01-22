db0788130f60792d8fe9c0f1860954e5bef37167
Tony <97451137+TonybynMp4@users.noreply.github.com>
2026-01-22 16:04:21 +0100

    chore: cleanup

--- packages/env/src/server.api.ts
+++ packages/env/src/server.api.ts
@@ -3,12 +3,13 @@ import "dotenv/config";
 import { z } from "zod";
 
 export const env = createEnv({
-	server: {
-		CORS_ORIGIN: z.url().default("http://localhost:80"),
-		PORT: z.coerce.number().int().min(1).max(65535).default(3000),
-		DATABASE_URL: z.string().min(1),
-		NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
-	},
-	runtimeEnv: process.env,
-	emptyStringAsUndefined: true,
+    server: {
+        CORS_ORIGIN: z.url().default("http://localhost:80"),
+        PORT: z.coerce.number().int().min(1).max(65535).default(3000),
+        DATABASE_URL: z.string().min(1),
+        UPLOADTHING_API_KEY: z.string().min(1),
+        NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
+    },
+    runtimeEnv: process.env,
+    emptyStringAsUndefined: true,
 });
